7806da07762b9378f11a82448e5c1ed7cac1fc6f
Jiajian Wu <jair.wu@rock-chips.com>
2024-08-30 16:29:36 +0800

    rk_demo: coffee_machine: Picture always center align
    Change-Id: I74ef48c6ddd6a298cb5c161dd256220c7e2d9364
    Signed-off-by: Jiajian Wu <jair.wu@rock-chips.com>

--- rk_demo/furniture_control/coffee_machine.c
+++ rk_demo/furniture_control/coffee_machine.c
@@ -42,34 +42,34 @@ void coffee_machine_png_box_scroll_callback(lv_event_t *event)
     lv_obj_t *screen = lv_event_get_target(event);
     scroll_value = lv_obj_get_scroll_left(screen);
     //printf("%d pixels are scrolled out on the left\n", scroll_value);
-    if (scroll_value > 0 && scroll_value < 250)
+    if (scroll_value > 0 && scroll_value <= 200)
     {
         lv_img_set_zoom(coffee_1, 256);
         lv_img_set_zoom(coffee_2, 192);
         lv_label_set_text(coffee_machine_name, "美式咖啡");
     }
-    if (scroll_value > 250 && scroll_value < 650)
+    if (scroll_value > 200 && scroll_value <= 600)
     {
         lv_img_set_zoom(coffee_1, 192);
         lv_img_set_zoom(coffee_2, 256);
         lv_img_set_zoom(coffee_3, 192);
         lv_label_set_text(coffee_machine_name, "意式咖啡");
     }
-    if (scroll_value > 650 && scroll_value < 1050)
+    if (scroll_value > 600 && scroll_value <= 1000)
     {
         lv_img_set_zoom(coffee_2, 192);
         lv_img_set_zoom(coffee_3, 256);
         lv_img_set_zoom(coffee_4, 192);
         lv_label_set_text(coffee_machine_name, "美味拿铁");
     }
-    if (scroll_value > 1050 && scroll_value < 1450)
+    if (scroll_value > 1000 && scroll_value <= 1400)
     {
         lv_img_set_zoom(coffee_3, 192);
         lv_img_set_zoom(coffee_4, 256);
         lv_img_set_zoom(coffee_5, 192);
         lv_label_set_text(coffee_machine_name, "卡布奇诺");
     }
-    if (scroll_value > 1450 && scroll_value < 1850)
+    if (scroll_value > 1400 && scroll_value <= 1800)
     {
         lv_img_set_zoom(coffee_4, 192);
         lv_img_set_zoom(coffee_5, 256);
@@ -101,24 +101,22 @@ void coffee_machine_ui_init(void)
     lv_obj_set_flex_flow(coffee_machine_box, LV_FLEX_FLOW_COLUMN);//列
 
     coffee_machine_png_box = lv_obj_create(coffee_machine_box);
+    lv_obj_set_scroll_snap_x(coffee_machine_png_box, LV_SCROLL_SNAP_CENTER);
     lv_obj_remove_style_all(coffee_machine_png_box);
     lv_obj_set_width(coffee_machine_png_box, lv_pct(100));
     lv_obj_set_height(coffee_machine_png_box, 300);
+    lv_obj_set_style_pad_hor(coffee_machine_png_box, (scr_w - 300) / 2, 0);
     if (coffee_machine_png_box != NULL)
     {
         lv_obj_add_event_cb(coffee_machine_png_box,
-                            coffee_machine_png_box_scroll_callback, LV_EVENT_SCROLL, NULL);
+                            coffee_machine_png_box_scroll_callback,
+                            LV_EVENT_SCROLL, NULL);
     }
     //lv_obj_set_style_radius(coffee_machine_png_box, LV_RADIUS_CIRCLE, 0);
     //lv_obj_set_style_clip_corner(coffee_machine_png_box, true, 0);
 
     //图片缩放 lv_img_set_zoom
     //设置咖啡图片
-    coffee_0 = lv_img_create(coffee_machine_png_box);
-    lv_obj_set_width(coffee_0, 300);
-    lv_obj_set_height(coffee_0, 300);
-    lv_obj_align(coffee_0, LV_ALIGN_CENTER, -400, 10);
-
     coffee_1 = lv_img_create(coffee_machine_png_box);
     lv_obj_set_width(coffee_1, 300);
     lv_obj_set_height(coffee_1, 300);
@@ -148,11 +146,6 @@ void coffee_machine_ui_init(void)
     lv_obj_align(coffee_5, LV_ALIGN_CENTER, 1600, 10);
     lv_img_set_src(coffee_5, COFFEE_5);
     lv_img_set_zoom(coffee_5, 192);
-
-    coffee_6 = lv_img_create(coffee_machine_png_box);
-    lv_obj_set_width(coffee_6, 300);
-    lv_obj_set_height(coffee_6, 300);
-    lv_obj_align(coffee_6, LV_ALIGN_CENTER, 2000, 10);
     //设置咖啡图片
 
     coffee_machine_box_name = lv_obj_create(coffee_machine_box);
